8b4da368f21b96db9d72ef2e578caaf28609bb3b
yunssy <yunssy@AA-01-062.local>
2026-07-27 14:12:16 +0800

    Add questions

--- questions/stage4.js
+++ questions/stage4.js
@@ -2,14 +2,74 @@
 window.DB_STAGE_4 = [
   {
     id: 1,
-    time: 60,
+    time: 10,
     text: "16, 25, 36, 49, [ ? ]",
-    answer: "64"
+    answer: "64",
   },
   {
     id: 2,
-    time: 60,
+    time: 30,
     text: "1, 1, 2, 3, 5, 8, [ ? ]",
-    answer: "13"
-  }
+    answer: "13",
+  },
+  {
+    id: 3,
+    time: 10,
+    text: "2, 4, 6, 8, [ ? ]",
+    answer: "10",
+  },
+  {
+    id: 4,
+    time: 20,
+    text: "5, 10, 20, 40, [ ? ]",
+    answer: "80",
+  },
+  {
+    id: 5,
+    time: 20,
+    text: "3, 6, 12, 24, [ ? ]",
+    answer: "48",
+  },
+  {
+    id: 6,
+    time: 5,
+    text: "100, 90, 80, 70, [ ? ]",
+    answer: "60",
+  },
+  {
+    id: 7,
+    time: 60,
+    text: "1, 3, 6, 10, 15, [ ? ]",
+    answer: "21",
+  },
+  {
+    id: 8,
+    time: 60,
+    text: "64, 32, 16, 8, [ ? ]",
+    answer: "44",
+  },
+  {
+    id: 9,
+    time: 20,
+    text: "81, 64, 49, 36, [ ? ]",
+    answer: "25",
+  },
+  {
+    id: 10,
+    time: 60,
+    text: "A=1、B=2、C=3⋯⋯，CAT = ?",
+    answer: "24",
+  },
+  {
+    id: 11,
+    time: 60,
+    text: "2, 6, 12, 20, 30, [ ? ]",
+    answer: "42",
+  },
+  {
+    id: 12,
+    time: 60,
+    text: "2, 5, 8, 11, [ ? ]",
+    answer: "14",
+  },
 ];
